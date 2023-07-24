0ff299f0c651ad7e79addd870e361ffc4260af0b
Dmitry Uchkin <not3hunnit@icloud.com>
2023-07-24 21:23:56 +0300

    Done

--- src/scripts/Popup.js
+++ src/scripts/Popup.js
@@ -1,14 +1,15 @@
 export class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', evt => this._handleEscClose(evt));
+    document.addEventListener('keydown', this._handleEscClose);
   }
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', evt => this.open());
+    document.removeEventListener('keydown', this._handleEscClose);
   }
   _handleEscClose(evt) {
     if (evt.key === 'Escape') {
